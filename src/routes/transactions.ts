7482eba7d2d7adf42b66669a281af5939b555ce6
Jandir A. Cutabiala <jandir.dev@gmail.com>
2023-06-18 13:27:28 -0300

    feat: get transactions by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,8 +5,20 @@ import { randomUUID } from "node:crypto";
 import { knex } from "../database";
 
 export async function transactionsRoutes (app: FastifyInstance): Promise<void> {
-  app.get("/", async () => {
-    return (await knex("transactions").select("*"));
+  app.get("/", async (_, __) => {
+    const transactions = await knex("transactions").select("*");
+
+    return { transactions };
+  });
+
+  app.get("/:id", async (request, __) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid()
+    });
+
+    const { id } = getTransactionsParamsSchema.parse(request.params);
+
+    return (await knex("transactions").where("id", id).first());
   });
 
   app.post("/", async (request, reply) => {
